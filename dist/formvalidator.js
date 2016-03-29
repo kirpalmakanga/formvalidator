3cb248c30acc90ca4a87bddc81c43805bf040c30
kirpalmakanga <makanga.kirpal@gmail.com>
2016-03-29 18:29:35 +0200

    optimized beforeSending call and added multiple forms init

--- dist/formvalidator.js
+++ dist/formvalidator.js
@@ -57,8 +57,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     }
   }
 
-  function send(settings) {
-    var form = document.querySelector(settings.form);
+  function send(form, settings) {
 
     callback(settings.beforeSending, form, function () {
       var action = form.getAttribute('data-form-action') ? form.getAttribute('data-form-action') : form.getAttribute('action');
@@ -81,9 +80,13 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
     });
   }
 
-  function setListeners(settings) {
-    var form = document.querySelector(settings.form);
+  function setListeners(form, settings) {
     var submit = form.querySelector('[type="submit"]');
+    var submitFunc = settings.ajax ? function () {
+      return send(form, settings);
+    } : function () {
+      return form.submit();
+    };
     var inputs = [].slice.call(form.querySelectorAll('[required]'));
     var inputEvents = function inputEvents(element) {
       var string = 'blur ';
@@ -106,17 +109,13 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
       });
     }
 
-    if (inputs.length > 1) {
-      inputs.forEach(function (input) {
-        addEventListeners(input, inputEvents(input), function () {
-          return validate(input, settings);
-        });
+    if (inputs.length > 1) inputs.forEach(function (input) {
+      return addEventListeners(input, inputEvents(input), function () {
+        return validate(input, settings);
       });
-    } else {
-      addEventListeners(inputs[0], inputEvents(inputs[0]), function () {
-        return validate(inputs[0], settings);
-      });
-    }
+    });else addEventListeners(inputs[0], inputEvents(inputs[0]), function () {
+      return validate(inputs[0], settings);
+    });
 
     submit.addEventListener('click', function (e) {
       var errors = inputs.reduce(function (sum, input) {
@@ -125,22 +124,16 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
       e.preventDefault();
 
-      if (errors) {
-        return false;
-      }
-
-      if (settings.ajax) {
-        send(settings);
-      } else {
-        callback(settings.beforeSending, form, function () {
-          return form.submit();
-        });
-      }
+      if (!errors) callback(settings.beforeSending, form, submitFunc);
     });
   }
 
   document.formValidator = function (options) {
     var settings = createSettings(options && (typeof options === 'undefined' ? 'undefined' : _typeof(options)) === 'object' ? options : {});
-    setListeners(settings);
+    var forms = [].slice.call(document.querySelectorAll(settings.form));
+
+    if (forms.length > 1) forms.forEach(function (form) {
+      return setListeners(form, settings);
+    });else setListeners(forms[0], settings);
   };
 })(document);
